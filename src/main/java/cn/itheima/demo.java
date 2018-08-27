49a955f694ef04c3c0c75f812498fd1a4ed28c8f
wxh <wuzhaohh163@163.com>
2018-08-27 18:46:14 +0800

    第一次提交

---
+++ src/main/java/cn/itheima/demo.java
@@ -0,0 +1,11 @@
+package cn.itheima;
+
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("------1111111----------");
+        System.out.println("-----222222-----");
+
+    }
+
+}
